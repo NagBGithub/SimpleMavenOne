c880198b68d42242b77fe3e4f03276271f4f6fe1
Nag B <centaur009@gmail.com>
2019-07-14 08:14:40 +0530

    First Commit

---
+++ src/test/java/TestPackage/HelloWorld.java
@@ -0,0 +1,21 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+    @Test
+    public void b() {
+        System.out.println(" Hello world Class 1");
+    }
+
+    @Test
+    public void b1() {
+        System.out.println(" Hello world Class 2 ");
+    }
+
+    @Test
+    public void b2() {
+        System.out.println(" Hello world Class 3 ");
+    }
+
+}
---
+++ src/test/java/TestPackage/HiWorld2.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class HiWorld2 {
+
+    @Test
+    public void a() {
+        System.out.println("Hi world Class");
+    }
+}
